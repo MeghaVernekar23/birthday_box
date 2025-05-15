01efa9fe271c292e11fd8d967f3b27ab5757da6c
MeghaVernekar23 <meghavernekar98@gmail.com>
2025-05-15 20:51:43 +0200

    Refactored App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
-import Login from "./components/Login";
-import Dashboard from "./components/Dashboard";
+import Login from "./pages/Login";
+import Dashboard from "./pages/Dashboard";
 
 import { Routes, Route } from "react-router-dom";
 
