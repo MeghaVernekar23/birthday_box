48d6378c978fefdc4aa469c232b4d99f36501369
MeghaVernekar23 <meghavernekar98@gmail.com>
2025-06-01 12:41:37 +0200

    Refactored main.py

--- backend/main.py
+++ backend/main.py
@@ -2,10 +2,42 @@ from fastapi import FastAPI
 from api.users import users_router
 from api.bookings import bookings_router
 from api.customers import customer_router
+from api.health import health_router
 from fastapi.middleware.cors import CORSMiddleware
+from collections.abc import AsyncGenerator
+from contextlib import asynccontextmanager
+from db.sessions import create_tables
 
-app = FastAPI(title="Birthday Box API implementation",
-    description="Birthday Box API implementation",)
+@asynccontextmanager
+async def lifespan(app: FastAPI) -> AsyncGenerator[None, None]:
+    create_tables()
+    yield
+
+app = FastAPI(
+    title="Birthday Box API",
+    version="0.1.0",
+    lifespan=lifespan,
+    description="API implementation for managing Birthday Box events, bookings, and customer data.",
+    contact={"name": "Megha Vernekar"},
+    openapi_tags=[
+        {
+            "name": "Health",
+            "description": "Minimal API for checking the backend health status.",
+        },
+        {
+            "name": "User",
+            "description": "APIs for user validations.",
+        },
+        {
+            "name": "Booking",
+            "description": "APIs for booking validations.",
+        },
+        {
+            "name": "Customer",
+            "description": "APIs for customer validations.",
+        },
+    ],
+)
 
 app.add_middleware(
     CORSMiddleware,
@@ -15,6 +47,7 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+app.include_router(health_router)
 app.include_router(users_router)
 app.include_router(bookings_router)
 app.include_router(customer_router)
