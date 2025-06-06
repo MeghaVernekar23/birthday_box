568eb1f098081180a9c381fbf4720cb161bcf807
MeghaVernekar23 <meghavernekar98@gmail.com>
2025-06-06 09:18:41 +0200

    Updated ORM mode to from_attribute

--- backend/db/models/booking_pydantic_model.py
+++ backend/db/models/booking_pydantic_model.py
@@ -23,7 +23,7 @@ class BookingDetails(BaseModel):
     updated_at: Optional[datetime] 
 
     class Config:
-        orm_mode = True    
+        from_attributes = True  
 
 
 class CelebrationDetails(BaseModel):
