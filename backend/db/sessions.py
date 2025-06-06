369c023e8fb4158784c4012c1276a6109df89522
MeghaVernekar23 <meghavernekar98@gmail.com>
2025-06-06 09:19:03 +0200

    Changed Db from SQLite to postgres

--- backend/db/sessions.py
+++ backend/db/sessions.py
@@ -1,21 +1,29 @@
-
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-
-from sqlalchemy.orm import declarative_base
-
+import os
+from sqlalchemy.orm import Session, declarative_base, sessionmaker
+from collections.abc import Generator
 
 Base = declarative_base()
 
-DATABASE_URL = "sqlite:///./birthdayBox.db"
-engine = create_engine(DATABASE_URL, echo=True)
+# Lazy-load engine and sessionmaker
+def get_engine():
+    DB_HOST = os.getenv("PG_HOST", "localhost")
+    DB_PORT = os.getenv("PG_PORT", "5433")
+    DB_NAME = os.getenv("PG_DATABASE", "birthday_box")
+    DB_USER = os.getenv("PG_USER", "postgres")
+    DB_PASS = os.getenv("PG_PASSWORD", "admin")
+    DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+    return create_engine(DATABASE_URL)
+
+engine = get_engine()
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
-
-def create_tables():
+def create_tables() -> None:
     Base.metadata.create_all(bind=engine)
 
-def get_db():
+    
+
+def get_db() -> Generator[Session, None, None]:
     db = SessionLocal()
     try:
         yield db
@@ -24,4 +32,4 @@ def get_db():
         db.rollback()
         raise
     finally:
-        db.close() 
+        db.close()
