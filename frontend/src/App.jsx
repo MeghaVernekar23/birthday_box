8192fbad28b1381b194a75e4b3229704960d14c8
MeghaVernekar23 <meghavernekar98@gmail.com>
2025-06-07 10:17:17 +0200

    Coomiting App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,7 +3,9 @@ import Layout from "./components/Layout";
 import Login from "./pages/Login";
 import Dashboard from "./pages/Dashboard";
 import CustomerDetails from "./pages/Customer";
-import Bookings from "./pages/Bookings";
+import TodaysBookings from "./pages/TodaysBookings";
+import UpcomingBookings from "./pages/UpcomingBookings";
+import OlderBookings from "./pages/OlderBookings";
 import AddBookings from "./pages/AddBookings";
 
 import { Routes, Route } from "react-router-dom";
@@ -17,7 +19,9 @@ function App() {
         <Route path="/" element={<Layout />}>
           <Route path="dashboard" element={<Dashboard />} />
           <Route path="customers" element={<CustomerDetails />} />
-          <Route path="bookings" element={<Bookings />} />
+          <Route path="/bookings/today" element={<TodaysBookings />} />
+          <Route path="/bookings/upcoming" element={<UpcomingBookings />} />
+          <Route path="/bookings/older" element={<OlderBookings />} />
           <Route path="addbooking" element={<AddBookings />} />
         </Route>
       </Routes>
