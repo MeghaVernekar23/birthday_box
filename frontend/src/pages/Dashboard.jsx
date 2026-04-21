7d8d6ea9dbf8537be4468defe932ec8acb13f2bf
MeghaVernekar23 <meghavernekar98@gmail.com>
2026-04-21 19:15:04 +0200

    feat: remove AvailableSlots and PaymentStatus cards from dashboard

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -2,8 +2,6 @@ import React from "react";
 import BookingSummary from "./BookingTakenSummary";
 import NextBooking from "./NextBooking";
 import UpcomingHolidaysCard from "./UpcomingHolidays";
-import AvailableSlots from "./AvailableSlots";
-import PaymentStatus from "./PaymentStatus";
 import CelebrationAndPackage from "./CelebrationAndPackage";
 import "../css/Dashboard.css";
 import AnalyticsSection from "./AnalyticsSection";
@@ -57,16 +55,7 @@ function Dashboard() {
 
       <div className="dashboard-grid-mid">
         <div className="dashboard-card-col">
-          <PaymentStatus />
-        </div>
-        <div className="dashboard-card-col">
           <CelebrationAndPackage />
-        </div>
-      </div>
-
-      <div className="dashboard-grid-bottom">
-        <div className="dashboard-card-col">
-          <AvailableSlots />
         </div>
       </div>
 
